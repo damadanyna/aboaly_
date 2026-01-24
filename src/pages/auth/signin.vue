f240bc8b951c63b67f2dc0201e56c38e5c167da8
DAMA Dany <damadanyna@gmail.com>
2026-01-24 12:48:08 +0300

    Changes to be committed: 	new file:   electron/main.js 	modified:   package-lock.json 	modified:   package.json 	renamed:    src/pages/auth/login.vue -> src/pages/auth/signin.vue 	renamed:    src/pages/auth/sigin.vue -> src/pages/auth/signup.vue 	modified:   src/pages/index.vue 	modified:   vite.config.mjs

--- src/pages/auth/signin.vue
+++ src/pages/auth/signin.vue
@@ -1,16 +1,25 @@
 <template>
 <div id="bg_img" class="  w-full flex flex-row justify-end items-center h-full">
 
-    <div class="flex   justify-end flex-row px-24 w-[80vw]">
-        <div class="  w-[50%] pa-12">
+    <div class="flex justify-end flex-row sm:px-24 sm:w-[80vw] ">
+        <div class="  sm:w-[50%] pa-12 " >
             <div class=" mb-10">
                 <span class=" text-cyan-400 text-3xl font-bold">Login</span>
             </div>
             <v-text-field v-for="item,i in user_form" :key="i" :label="item.title" v-model="item.field" variant="outlined" :prepend-inner-icon="item.icon"></v-text-field>
 
-            <v-btn :loading="loading" class="flex-grow-1" height="48" variant="tonal" @click="load">
-                Se Connecter
-            </v-btn>
+            <div class="flex flex-col">
+                <v-btn :loading="loading" class="flex-grow-1" height="48" variant="tonal" @click="load">
+                    Se Connecter
+                </v-btn>
+                <div class=" flex justify-end items-center mt-4  ">
+                Vous n'avez pas de compte
+                <button @click="inscription_rout()" variant=" outlined" class=" ml-3 underline text-cyan-500" >
+                  S'inscrire
+                </button>
+                </div>
+
+            </div>
 
         </div>
     </div>
@@ -19,8 +28,12 @@
 
 <script setup>
 import {
-    ref
+    ref,
 } from 'vue'
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 const user_form = ref(
     [{
             field: "",
@@ -43,6 +56,10 @@ const loading = ref(false)
 const load = () => {
     loading.value = true
     setTimeout(() => (loading.value = false), 3000)
+}
+const inscription_rout = () => {
+  router.push('signup')
+
 }
 </script>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup>
+
+import { useRouter } from 'vue-router'
+import {
+    ref,
+} from 'vue'
+
+
+const router = useRouter()
   //
+  onMounted(() => {
+    router.replace('/auth/signin')
+  })
 </script>
